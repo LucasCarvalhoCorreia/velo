6f4e8078aef2ae99b5a30da5b42610c6fca15ec9
Lucas Carvalho <lucas.carvalho79@hotmail.com>
2026-01-28 19:07:48 -0300

    Adicionando comentario de padrão AAA no código

--- playwriht/e2e/pedidos.spec.ts
+++ playwriht/e2e/pedidos.spec.ts
@@ -1,19 +1,19 @@
 import { test, expect } from '@playwright/test';
 
+/// AAA - Arrange, Act, Assert
+
 test('deve consultar um pedido aprovado', async ({ page }) => {
+  // Arrange
   await page.goto('http://localhost:5173/');
-
-  // Checkpoint
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
-
   await page.getByRole('link', { name: 'Consultar Pedido' }).click();
-  
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
 
+  // Act
   await page.getByTestId('search-order-id').fill('VLO-MMTX2T');
-
   await page.getByTestId('search-order-button').click();
 
+  // Assert
   await expect(page.getByTestId('order-result-id')).toBeVisible();
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-MMTX2T');
 
