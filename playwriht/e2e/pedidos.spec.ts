35a1a87f050e8363130a7eb1124223e556dfdbbb
Lucas Carvalho <lucas.carvalho79@hotmail.com>
2026-01-30 19:48:18 -0300

    Ajustando mapeamento de elementos e adicionando timeout

--- playwriht/e2e/pedidos.spec.ts
+++ playwriht/e2e/pedidos.spec.ts
@@ -10,11 +10,11 @@ test('deve consultar um pedido aprovado', async ({ page }) => {
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
 
   // Act
-  await page.getByTestId('search-order-id').fill('VLO-MMTX2T');
+  await page.getByRole('textbox', { name: 'Número do Pedido' }).fill('VLO-MMTX2T');
   await page.getByTestId('search-order-button').click();
 
   // Assert
-  await expect(page.getByTestId('order-result-id')).toBeVisible();
+  await expect(page.getByTestId('order-result-id')).toBeVisible({ timeout: 10_000 });
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-MMTX2T');
 
   await expect(page.getByTestId('order-result-status')).toBeVisible();
