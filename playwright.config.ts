1560784ece1f42b359661799ad904cfd16a86abc
Lucas Carvalho <lucas.carvalho79@hotmail.com>
2026-01-30 20:16:16 -0300

    Ajuste de nomes de pasta

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,7 +13,7 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   timeout: 30000,
-  testDir: './playwriht/e2e',
+  testDir: './playwright/e2e',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
