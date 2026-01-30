6e142d6b80ae71308ea25962a8bc2fa81b47fc99
Lucas Carvalho <lucas.carvalho79@hotmail.com>
2026-01-30 20:05:22 -0300

    Desafio 1

--- playwriht/e2e/pedidos.spec.ts
+++ playwriht/e2e/pedidos.spec.ts
@@ -6,17 +6,18 @@ test('deve consultar um pedido aprovado', async ({ page }) => {
   // Arrange
   await page.goto('http://localhost:5173/');
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
+  
   await page.getByRole('link', { name: 'Consultar Pedido' }).click();
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
 
   // Act
   await page.getByRole('textbox', { name: 'Número do Pedido' }).fill('VLO-MMTX2T');
-  await page.getByTestId('search-order-button').click();
+  await page.getByRole('button', { name: 'Buscar Pedido' }).click();
 
   // Assert
-  await expect(page.getByTestId('order-result-id')).toBeVisible({ timeout: 10_000 });
-  await expect(page.getByTestId('order-result-id')).toContainText('VLO-MMTX2T');
+  await expect(page.getByText('PedidoVLO-MMTX2T')).toBeVisible();
+  await expect(page.getByTestId('order-result-VLO-MMTX2T')).toContainText('PedidoVLO-MMTX2T');
 
-  await expect(page.getByTestId('order-result-status')).toBeVisible();
-  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
+  await expect(page.getByText('APROVADO')).toBeVisible();
+  await expect(page.getByTestId('order-result-VLO-MMTX2T')).toContainText('APROVADO');
 })
