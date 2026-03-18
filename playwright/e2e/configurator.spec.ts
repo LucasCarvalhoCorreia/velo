25e3b243aa5e579afdfb24754689b688332bd528
Lucas Carvalho <lucas.carvalho79@hotmail.com>
2026-03-17 22:02:47 -0300

    Retirando comentarios

--- playwright/e2e/configurator.spec.ts
+++ playwright/e2e/configurator.spec.ts
@@ -26,24 +26,19 @@ test.describe('Configuração do Veículo', () => {
   })
 
   test('CT03 - deve atualizar o preço ao adicionar opcionais e navegar para o checkout com os valores corretos', async ({ app }) => {
-    // Estado inicial sem opcionais
     await app.configurator.expectPrice('R$ 40.000,00')
 
-    // 1 - Adicionar Precision Park
     await app.configurator.toggleOptional('Precision Park')
     await app.configurator.expectPrice('R$ 45.500,00')
 
-    // 2 - Adicionar Flux Capacitor
     await app.configurator.toggleOptional('Flux Capacitor')
     await app.configurator.expectPrice('R$ 50.500,00')
 
-    // 3 - Desmarcar os checkboxes dos opcionais
     await app.configurator.toggleOptional('Precision Park')
     await app.configurator.expectPrice('R$ 45.000,00')
     await app.configurator.toggleOptional('Flux Capacitor')
     await app.configurator.expectPrice('R$ 40.000,00')
 
-    // 4 - Ir para o checkout com a configuração atual
     await app.configurator.goToCheckout('R$ 40.000,00')
   })
 })
